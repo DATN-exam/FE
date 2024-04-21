b2aea1fca6906997f930453b5038cff6b92edab6
ToanND-kozocom <toannd@kozo-japan.com>
2024-04-21 12:10:03 +0700

    manage student

--- src/config/define.ts
+++ src/config/define.ts
@@ -30,3 +30,32 @@ export const ROLE = {
   STUDENT: 'STUDENT',
   TEACHER: 'TEACHER',
 }
+
+export const AUTH_VERIFY_STATUS = {
+  LOADING: 'loading',
+  NOT_VERIFIED: 'not-verified',
+  VERIFIED: 'verified',
+}
+
+export const TEACHER_STATUS_LIST = [
+  {
+    id: 1,
+    name: 'Chờ xác nhận',
+    value: 'WAIT_VERIFY',
+  },
+  {
+    id: 2,
+    name: 'Đang hoạt động',
+    value: 'ACTIVE',
+  },
+  {
+    id: 3,
+    name: 'Riêng tư',
+    value: 'BLOCK',
+  },
+  {
+    id: 4,
+    name: 'Bị khóa',
+    value: 'ADMIN_BLOCK',
+  },
+]
