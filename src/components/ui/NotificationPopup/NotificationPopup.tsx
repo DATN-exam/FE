fd22a16c22f5947a33fec9164902d6c0a9676345
ToanND-Kozocom <toannd@kozo-japan.com>
2024-06-22 09:45:53 +0700

    update notification

--- src/components/ui/NotificationPopup/NotificationPopup.tsx
+++ src/components/ui/NotificationPopup/NotificationPopup.tsx
@@ -5,8 +5,8 @@ function NotificationPopup(props: any) {
   const { message } = props
 
   return (
-    <div className="flex justify-between mt-3">
-      <h3 className="text-xl items-center">{message}</h3>
+    <div className="flex justify-between mt-3 select-none">
+      <h3 className="text-xl items-center select-none">{message}</h3>
       <Link to={ROUTES_SITE.ACTIVITY} className="text-blue-500">
         Xem tất cả thông báo
       </Link>
